f5602fea458eb2e179450c1d1735dd57ecdaad93
1634837963@qq.com <lg19970122>
2018-10-09 08:27:11 +0800

    定位完成——log消息打印

--- app/src/main/java/com/cqeec/by/sos/MainActivity.java
+++ app/src/main/java/com/cqeec/by/sos/MainActivity.java
@@ -1,24 +1,29 @@
 package com.cqeec.by.sos;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.cqeec.by.sos.Const;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    //绑定空间
+    private TextView position_textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar =findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        
+        BaiduDw baiduDw = new BaiduDw(MainActivity.this);
+        baiduDw.doLocation();//开启定位
+        baiduDw.mLocationClient.restart();//开始定位,异常情况重启定位
+
 
     }
 
