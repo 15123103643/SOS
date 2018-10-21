fbabe5b7ed8d3429452b52fbdca25c61a00cc5c8
罗玉刚 <1634837963@qq.com>
2018-10-21 14:52:12 +0800

    用户个人信息开发2

--- app/src/main/java/com/cqeec/by/sos/UserActivity.java
+++ app/src/main/java/com/cqeec/by/sos/UserActivity.java
@@ -1,13 +1,63 @@
 package com.cqeec.by.sos;
-
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import org.litepal.LitePal;
 
 public class UserActivity extends AppCompatActivity {
+    private Button bback ,bsave;
+    private EditText user_name,user_sfzh,user_jtdz,user_xx,user_gmfy,user_zdy;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // LitePal.initialize(this);
         setContentView(R.layout.activity_user);
+        ActionBar();
+        Edit();
+        bsave();
+
+    }
+    public void Edit(){
+        user_name =findViewById(R.id.username);
+        user_sfzh=findViewById(R.id.sfzh);
+        user_jtdz=findViewById(R.id.jtdz);
+        user_xx=findViewById(R.id.xx);
+        user_gmfy=findViewById(R.id.gmfy);
+        user_zdy=findViewById(R.id.zdy);
+    }
+    public void  bsave(){
+        bsave = findViewById(R.id.bsave);
+        bsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                UserDB db = new UserDB();
+                db.setU_name(user_name.getText().toString());
+                db.setU_sfzh(user_sfzh.getText().toString());
+                db.setU_jtdz(user_jtdz.getText().toString());
+                db.setU_xx(user_xx.getText().toString());
+                db.setU_gmfy(user_gmfy.getText().toString());
+                db.setU_zdy(user_zdy.getText().toString());
+                Log.i("用户数据",user_name.getText().toString());
+                db.save();
+            }
+        });
+
+    }
+    public void ActionBar(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+
+        }
     }
 }
---
+++ app/src/main/java/com/cqeec/by/sos/Userjjlxr.java
@@ -0,0 +1,31 @@
+package com.cqeec.by.sos;
+
+public class Userjjlxr {
+    private String U_name;
+    private String U_phone;
+    private String U_email;
+
+    public String getU_name() {
+        return U_name;
+    }
+
+    public void setU_name(String u_name) {
+        U_name = u_name;
+    }
+
+    public String getU_phone() {
+        return U_phone;
+    }
+
+    public void setU_phone(String u_phone) {
+        U_phone = u_phone;
+    }
+
+    public String getU_email() {
+        return U_email;
+    }
+
+    public void setU_email(String u_email) {
+        U_email = u_email;
+    }
+}
