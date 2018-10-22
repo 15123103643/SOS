6438e8f351c87dedcbe64503bc0c200275d44b8b
cyl <cyl>
2018-10-22 11:36:44 +0800

    添加

---
+++ app/src/main/java/com/cqeec/by/sos/ContactsActivity.java
@@ -0,0 +1,26 @@
+package com.cqeec.by.sos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ContactsActivity  extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+        btn=(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 给bnt1添加点击响应事件
+                Intent intent =new Intent(ContactsActivity.this,AddContacts.class);
+                //启动
+                startActivity(intent);
+            }
+        });
+    }
+}
